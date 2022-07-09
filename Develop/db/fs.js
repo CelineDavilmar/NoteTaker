5849a5f264e2d2b4c1a4cbabe3a0ca004095b08f
Celine Davilmar <rdavi106@fiu.edu>
2022-07-09 11:18:20 -0400

    updated routes

--- Develop/db/fs.js
+++ Develop/db/fs.js
@@ -28,10 +28,15 @@ router.get("*", function (req, res) {
 //GET / api / notes should read the db.json file and return all saved notes as JSON.
 //POST / api / notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.You'll need to find a way to give each note a unique id when it's saved(look into npm packages that could do this for you). */
 
+function addNote(title, description) {
+    Note.push(title = req.Note.title, description = req.Note.body)
+    req.id = id.uniqid
+}
+
 router.get("/api/notes", (req, res) => {
     res
         .getNotes(req.body)
-        .then(notes => res.json(notes))
+        .then(note => res.json(note))
         .catch(err => res.status(500).json(err));
 });
 
@@ -43,10 +48,8 @@ router.post("api/notes", (req, res) => {
     Note.description = req.body.description
 
     res
-        .addNote(req.Note.title, req.Note.description)
-        .req.id = id.uniqid
-            .then((note) => res.json(note))
-            .catch(err => res.status(500).json(err));
+    addNote(req.Note.title, req.Note.description)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
 });
-
 
