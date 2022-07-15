09dc660b758716148a6a1ff7e78c8777136d3781
Celine Davilmar <rdavi106@fiu.edu>
2022-07-14 21:46:31 -0400

    updates

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Note Taker 
 
 # Purpose
-
+    Allows a user to input text for a note and save the note.
 # Built With
-
+    HTML
+    JS
+    CSS
 
 # Screenshot of Project
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 //const path = require('path');
 //const session = require('express-session');
-const router = require('./miniature-eureka/Develop/db/fs');
+const router = require('./Develop/db/fs');
 const exphbs = require('express-handlebars');
 
 const app = express();
